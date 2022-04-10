3f8fcaf119ced4404ca14d9681fde7fa61b4c9f5
markedboat <ahyjl@126.com>
2022-04-11 02:28:39 +0800

    try remove

--- js/tampermonkey/_release/singly/music.163.com/dst/index.js
+++ js/tampermonkey/_release/singly/music.163.com/dst/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hammerWeb2TV-网易云音乐
 // @namespace    http://tampermonkey.net/
-// @version      0.20220411003659
+// @version      0.20220411021838
 // @description  在电视上听使用网易云,目前只能听歌…… https://markedboat.com/cors/tampermonkey_release?browser=kiwi&name=music.163.com
 // @author       You
 // @include      https://music.163.com/*
@@ -10,8 +10,8 @@
 // @grant window.close
 // @grant window.focus
 // @run-at document-start
-// @updateURL    https://markedboat.com/cors/js_file?file=tampermonkey/_release/singly/music.163.com/res.js
-// @downloadURL  https://markedboat.com/cors/js_file?file=tampermonkey/_release/singly/music.163.com/res.js
+// @updateURL    https://markedboat.com/cors/js_file?file=tampermonkey/_release/singly/music.163.com/dst/index.js
+// @downloadURL  https://markedboat.com/cors/js_file?file=tampermonkey/_release/singly/music.163.com/dst/index.js
 // ==/UserScript==
 
 (function () {
@@ -48,29 +48,15 @@
     // });
 
 
-    window.setTimeout(function () {
-        console.log(((self == top) ? 'top' : 'iframe') + (':' + document.location.href) + "\n" + '都15秒了，不能把浏览器写炸了,清理 kiwi_js_xhr_record_data,为了保险，延迟10秒');//定时器的number 会重复，所以改用随机数
-        console.log('kl_kiwi_js_xhr_record_data_default_cleaner', localStorage.getItem('kl_kiwi_js_xhr_record_data_default_cleaner'));
-
-        if (auto_clean == false) {
-            console.log('kl_kiwi_js_xhr_record_data_default_cleaner === false ,不清理');
-            return false;
-        } else {
-            console.log('kl_kiwi_js_xhr_record_data_default_cleaner !== false ,清理！！！');
+    let is_xhr_page = false;
+    ['song?id=', 'playlist?id='].map(function (tmp_url) {
+        if (is_xhr_page === false) {
+            is_xhr_page = document.location.href.indexOf(tmp_url) > 0;
         }
-        stopRecordXhr();
-        setTimeout(function () {
-            for (let i = 0; i < localStorage.length; i++) {
-                let key = localStorage.key(i); //获取本地存储的Key
-                if (key.indexOf('kiwi_js_xhr_') === 0) {
-                    localStorage.removeItem(key);
-                }
-            }
-        }, 10 * 1000);
-    }, 15 * 1000);
+    });
+    console.log(is_xhr_page);
 
-
-    if (document.location.href.indexOf('https://music.163.com/login') != 0 && document.location.href.indexOf('https://music.163.com/#/login') !== 0 && XMLHttpRequest.prototype.realOpen3 === undefined) {//网易云自己也改写了个realOpen……  重名了
+    if (is_xhr_page && XMLHttpRequest.prototype.realOpen3 === undefined) {//网易云自己也改写了个realOpen……  重名了
         XMLHttpRequest.prototype._kl_info = {url: '', method: '', sync: false, headers: []};
         console.log('XMLHttpRequest.prototype.realOpen3', XMLHttpRequest.prototype.realOpen3);
         XMLHttpRequest.prototype.realSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
@@ -115,11 +101,34 @@
                         }
                     }, true);
                     tmp_xhr.realSend(value);
+                    xhr._kl_info.headers=[];
                 }
             });
             this.realSend(value);
             this._kl_info.headers = [];
         };
+
+
+        window.setTimeout(function () {
+            console.log(((self == top) ? 'top' : 'iframe') + (':' + document.location.href) + "\n" + '都15秒了，不能把浏览器写炸了,清理 kiwi_js_xhr_record_data,为了保险，延迟10秒');//定时器的number 会重复，所以改用随机数
+            console.log('kl_kiwi_js_xhr_record_data_default_cleaner', localStorage.getItem('kl_kiwi_js_xhr_record_data_default_cleaner'));
+
+            if (auto_clean == false) {
+                console.log('kl_kiwi_js_xhr_record_data_default_cleaner === false ,不清理');
+                return false;
+            } else {
+                console.log('kl_kiwi_js_xhr_record_data_default_cleaner !== false ,清理！！！');
+            }
+            stopRecordXhr();
+            setTimeout(function () {
+                for (let i = 0; i < localStorage.length; i++) {
+                    let key = localStorage.key(i); //获取本地存储的Key
+                    if (key.indexOf('kiwi_js_xhr_') === 0) {
+                        localStorage.removeItem(key);
+                    }
+                }
+            }, 10 * 1000);
+        }, 15 * 1000);
 
     }
 
@@ -130,9 +139,9 @@
     }
 
 
-    window.localStorage.setItem('close_current_window', 'false');
     setInterval(function () {
-        if (window.localStorage.getItem('close_current_window') === 'true') {
+        if (top && top.window && top.window.kl && top.window.kl.closeCurrentWindow === true) {
+            console.log('尝试关闭 closeCurrentWindow');
             window.close();
         }
     }, 200);
@@ -153,11 +162,19 @@
         return document.realCreateElement(tagName);
     };
 
+
+    /*************************************************************************************************
+     *
+     * 引入业务相关
+     *
+     *************************************************************************************************/
     console.log('开始引入');
 
+    try {
 
-    console.log('hammer/kl-hammer.js');
-    /**
+        console.log('hammer/kl-hammer.js');
+
+        /**
  * Created by markedboat on 2019/12/19.
  */
 
@@ -506,105 +523,109 @@ function domLoaded(fn) {
 
 console.log('loaded hammer.js');
 
-    let alertMsg = function (msg) {
-        kl.log(msg);
-        alert(msg);
-    };
 
-    let timerCfg = {
-        flagMap: {},
-        triggers: [],
-        triggerMap: {},
-        counts: 0,
-    };
+        console.log('tampermonkey/_release/singly/music.163.com/src/common.js');
 
-    /**
-     *
-     * @param flag   执行之前会检查flag, true：执行  false:取消
-     * @param start 多少 n*0.1秒后执行,不受size影响
-     * @param ttl   从上面计多少次结束,不受size影响
-     * @param interval   时间间隔，精度为0.1秒，可能有用的不多的
-     * @param in_fun   在世界范围执行什么方法
-     * @param out_fun 退出时执行什么方法？
-     * {flag: flag, start: start, interval: interval, ttl: ttl, in_fun: dd, out_fun: fun,is_close:false}
-     *
-     */
-    let addTimerTrigger = function (input_param) {
-        if (timerCfg.flagMap[input_param.flag] === undefined) {
-            timerCfg.flagMap[input_param.flag] = input_param.is_close === undefined ? true : !input_param.is_close;
-            let tmp_index = timerCfg.triggers.length;
-            timerCfg.triggers.push(
-                {
-                    flag: input_param.flag,
-                    start: input_param.start < 1 ? -1 : (timerCfg.counts + input_param.start),
-                    end: input_param.ttl === -1 ? -1 : (timerCfg.counts + input_param.start + (input_param.ttl === 0 ? input_param.interval : input_param.ttl)),
-                    mod: input_param.interval,
-                    inFun: input_param.in_fun,
-                    outFun: input_param.out_fun,
-                    closeFlag: function () {
-                        console.log('closeFlag:', input_param.flag);
-                        timerCfg.flagMap[input_param.flag] = false;
-                    },
-                    openFlag: function () {
-                        console.log('openFlag:', input_param.flag);
-                        timerCfg.flagMap[input_param.flag] = true;
-                    },
-                    isFlagOpen: function () {
-                        return timerCfg.flagMap[input_param.flag];
-                    }
-                }
-            );
-            timerCfg.triggers[tmp_index].setNewTtl = function (start, ttl, in_fun, out_fun) {
-                timerCfg.triggers[tmp_index].start = start < 1 ? -1 : (timerCfg.counts + start);
-                timerCfg.triggers[tmp_index].end = ttl === -1 ? -1 : (timerCfg.counts + start + (ttl === 0 ? timerCfg.triggers[tmp_index].interval : ttl));
-                timerCfg.flagMap[input_param.flag] = true;
-                if (typeof in_fun === 'function') {
-                    timerCfg.triggers[tmp_index].inFun = in_fun;
-                }
-                if (typeof out_fun === 'function') {
-                    timerCfg.triggers[tmp_index].outFun = out_fun;
-                }
-                return timerCfg.triggers[tmp_index];
-            };
-            timerCfg.triggerMap[input_param.flag] = timerCfg.triggers[tmp_index];
-            return timerCfg.triggers[tmp_index];
-        } else {
-            throw  'addTimerTrigger 已经存在:' + input_param.flag;
-        }
-    };
+        let alertMsg = function (msg) {
+    kl.log(msg);
+    alert(msg);
+};
 
-    let isTimerTriggerExist = function (flag) {
-        return !(timerCfg.flagMap[flag] === undefined);
-    };
-    let getTimerTrigger = function (flag) {
-        return timerCfg.triggerMap[flag];
-    };
 
-    setInterval(function () {
-        timerCfg.counts = timerCfg.counts + 1;
-        timerCfg.triggers.forEach(function (trigger, index) {
-            //  console.log(trigger.flag, timerCfg.flagMap[trigger.flag], trigger, timerCfg.counts,                "\nmod:", timerCfg.counts % trigger.mod === 0,                "\nnot yet:", trigger.start > -1 && trigger.start < timerCfg.counts,                "\nout:", trigger.end > -1 && trigger.end < timerCfg.counts);
-            if (trigger.isFlagOpen() && timerCfg.counts % trigger.mod === 0) {
-                //  console.log(trigger.flag, timerCfg.flagMap[trigger.flag], trigger, timerCfg.counts,                "\nmod:", timerCfg.counts % trigger.mod === 0,                "\nnot yet:", trigger.start > -1 && trigger.start < timerCfg.counts,                "\nout:", trigger.end > -1 && trigger.end < timerCfg.counts);
-                if (trigger.start > -1 && trigger.start < timerCfg.counts) {
-                    //不是 无限次的，又没到 开始的时间,-1  无限， 0，一次，
-                    //console.log('not yet:',);
-                    return false;
-                }
-                if (trigger.end > -1 && trigger.end < timerCfg.counts) {
-                    //console.log('outer:',);
-                    trigger.closeFlag();
-                    trigger.outFun(trigger, timerCfg.counts);
-                } else {
-                    //console.log('in:',);
-                    trigger.inFun(trigger, timerCfg.counts);
+let timerCfg = {
+    flagMap: {},
+    triggers: [],
+    triggerMap: {},
+    counts: 0,
+};
+
+/**
+ *
+ * @param flag   执行之前会检查flag, true：执行  false:取消
+ * @param start 多少 n*0.1秒后执行,不受size影响
+ * @param ttl   从上面计多少次结束,不受size影响
+ * @param interval   时间间隔，精度为0.1秒，可能有用的不多的
+ * @param in_fun   在世界范围执行什么方法
+ * @param out_fun 退出时执行什么方法？
+ * {flag: flag, start: start, interval: interval, ttl: ttl, in_fun: dd, out_fun: fun,is_close:false}
+ *
+ */
+let addTimerTrigger = function (input_param) {
+    if (timerCfg.flagMap[input_param.flag] === undefined) {
+        timerCfg.flagMap[input_param.flag] = input_param.is_close === undefined ? true : !input_param.is_close;
+        let tmp_index = timerCfg.triggers.length;
+        timerCfg.triggers.push(
+            {
+                flag: input_param.flag,
+                start: input_param.start < 1 ? -1 : (timerCfg.counts + input_param.start),
+                end: input_param.ttl === -1 ? -1 : (timerCfg.counts + input_param.start + (input_param.ttl === 0 ? input_param.interval : input_param.ttl)),
+                mod: input_param.interval,
+                inFun: input_param.in_fun,
+                outFun: input_param.out_fun,
+                closeFlag: function () {
+                    console.log('closeFlag:', input_param.flag);
+                    timerCfg.flagMap[input_param.flag] = false;
+                },
+                openFlag: function () {
+                    console.log('openFlag:', input_param.flag);
+                    timerCfg.flagMap[input_param.flag] = true;
+                },
+                isFlagOpen: function () {
+                    return timerCfg.flagMap[input_param.flag];
                 }
             }
-        });
-    }, 100);
+        );
+        timerCfg.triggers[tmp_index].setNewTtl = function (start, ttl, in_fun, out_fun) {
+            timerCfg.triggers[tmp_index].start = start < 1 ? -1 : (timerCfg.counts + start);
+            timerCfg.triggers[tmp_index].end = ttl === -1 ? -1 : (timerCfg.counts + start + (ttl === 0 ? timerCfg.triggers[tmp_index].interval : ttl));
+            timerCfg.flagMap[input_param.flag] = true;
+            if (typeof in_fun === 'function') {
+                timerCfg.triggers[tmp_index].inFun = in_fun;
+            }
+            if (typeof out_fun === 'function') {
+                timerCfg.triggers[tmp_index].outFun = out_fun;
+            }
+            return timerCfg.triggers[tmp_index];
+        };
+        timerCfg.triggerMap[input_param.flag] = timerCfg.triggers[tmp_index];
+        return timerCfg.triggers[tmp_index];
+    } else {
+        throw  'addTimerTrigger 已经存在:' + input_param.flag;
+    }
+};
+
+let isTimerTriggerExist = function (flag) {
+    return !(timerCfg.flagMap[flag] === undefined);
+};
+let getTimerTrigger = function (flag) {
+    return timerCfg.triggerMap[flag];
+};
+
+setInterval(function () {
+    timerCfg.counts = timerCfg.counts + 1;
+    timerCfg.triggers.forEach(function (trigger, index) {
+        //  console.log(trigger.flag, timerCfg.flagMap[trigger.flag], trigger, timerCfg.counts,                "\nmod:", timerCfg.counts % trigger.mod === 0,                "\nnot yet:", trigger.start > -1 && trigger.start < timerCfg.counts,                "\nout:", trigger.end > -1 && trigger.end < timerCfg.counts);
+        if (trigger.isFlagOpen() && timerCfg.counts % trigger.mod === 0) {
+            //  console.log(trigger.flag, timerCfg.flagMap[trigger.flag], trigger, timerCfg.counts,                "\nmod:", timerCfg.counts % trigger.mod === 0,                "\nnot yet:", trigger.start > -1 && trigger.start < timerCfg.counts,                "\nout:", trigger.end > -1 && trigger.end < timerCfg.counts);
+            if (trigger.start > -1 && trigger.start < timerCfg.counts) {
+                //不是 无限次的，又没到 开始的时间,-1  无限， 0，一次，
+                //console.log('not yet:',);
+                return false;
+            }
+            if (trigger.end > -1 && trigger.end < timerCfg.counts) {
+                //console.log('outer:',);
+                trigger.closeFlag();
+                trigger.outFun(trigger, timerCfg.counts);
+            } else {
+                //console.log('in:',);
+                trigger.inFun(trigger, timerCfg.counts);
+            }
+        }
+    });
+}, 100);
 
 
-    try {
+
         console.log('hammer/tv/grid.js');
 
         /********************************************************************************************************************************************************
@@ -1179,7 +1200,7 @@ if (top.window.kl.tvGridListening !== true) {
 
 
 
-        console.log('tampermonkey/_release/singly/music.163.com/src/index.js');
+        console.log('tampermonkey/_release/singly/music.163.com/src/layout.js');
 
         /********************************************************************************************************************************************************
  *   ___ ____ _  _ _  _ ____ _  _      _ _  _ ___  ____ _  _
@@ -1290,7 +1311,7 @@ if (top.window.kl.tvGridListening !== true) {
             {
                 text: '关闭页面', handleKey: 'test', type: 'fun', fun: function () {
                     // window.close();
-                    window.localStorage.setItem('close_current_window', 'true');
+                    top.window.kl.closeCurrentWindow = true;
                 }
             },
             {
@@ -1434,7 +1455,7 @@ if (top.window.kl.tvGridListening !== true) {
 )();
 
 
-        console.log('tampermonkey/_release/singly/music.163.com/src/website.js');
+        console.log('tampermonkey/_release/singly/music.163.com/src/content_provider.js');
 
         /********************************************************************************************************************************************************
  *  _  _ _  _  ___ _  ___        /| |    ---.       ___ ____ _  _
